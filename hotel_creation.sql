5d4b5ffb594b9d2001f86ad254e6fc52334a0bf8
benjaminkravets <bakravets@gmail.com>
2022-11-14 12:45:14 -0500

    add payment tables

--- hotel_creation.sql
+++ hotel_creation.sql
@@ -137,6 +137,29 @@ create table reservation (
     check_out_complete BOOLEAN
 )
 
+create table payment (
+    payment_pk INT PRIMARY KEY,
+    reservation_pk INT PRIMARY KEY,
+    FOREIGN KEY (reservation_pk)
+        REFERENCES reservation(reservation_pk)
+    payment_method_pk INT,
+    FOREIGN KEY (payment_method_pk)
+        REFERENCES payment_method(payment_method_pk),
+    payment_complete BOOLEAN,
+    price DECIMAL(6,2)
+)
+
+create table payment_method (
+    payment_method_pk INT PRIMARY KEY,
+    member_pk INT PRIMARY KEY,
+    FOREIGN KEY member_pk 
+        REFERENCES member(member_pk)
+    credit_card_number VARCHAR(16),
+    expiration_date DATETIME,
+    security_code INT(3)
+
+)
+
 insert into reservation VALUES ('1', '1', '1', '101', '11/20/2001 2:00', '11/21/2001', NULL, '1', '2', 'FALSE', 'FALSE')
 insert into hotel VALUES ('1', 'Parkway East', 'Bedford', 'PA', '8149772014', '8149772015', '11:30', '1:30', '3.4', 'EDT');
 insert into suite values ('1', TRUE, TRUE, TRUE, TRUE, TRUE, TRUE, TRUE, TRUE, TRUE, '2', '2', '2', FALSE);
